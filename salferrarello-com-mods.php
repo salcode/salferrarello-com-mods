049575f9a40f3e6b2f21db809765826fce231b10
Sal Ferrarello <sal@ironcodestudio.com>
2018-04-26 15:22:47 -0400

    Add handling for "Draft" category posts
    - Add warning alert to "Draft" category posts.
    - Exclude "Draft" category posts from homepage.
    
    See #1

--- salferrarello-com-mods.php
+++ salferrarello-com-mods.php
@@ -14,3 +14,40 @@ function sf_genesis_pre_load_favicon( $favicon_url ) {
 	$plugin_dir_path = plugin_dir_url(__FILE__);
     return $plugin_dir_path . 'images/favicon.ico';
 }
+
+/**
+ * Add a warning alert on Draft posts.
+ */
+add_action( 'genesis_entry_header', 'sf_genesis_draft_notice' );
+function sf_genesis_draft_notice() {
+	global $post;
+	if ( ! in_category( 'draft', $post ) ) {
+		// Make no changes.
+		return;
+	}
+?>
+<br />
+<div class="alert alert-warning" role="alert">
+	<strong>Warning!</strong>
+	This is a draft, not a finalized post. See <a href="https://salferrarello.com/draft-blog-posts/">full draft disclosure</a>.
+</div>
+<?php
+}
+
+/**
+ * Exclude Draft posts from the blog page (which is also my homepage).
+ */
+add_filter( 'pre_get_posts', 'sf_exclude_draft_category' );
+function sf_exclude_draft_category( $query ) {
+	if ( $query->is_home ) {
+		$query->set( 'tax_query', [
+			[
+				'taxonomy' => 'category',
+				'field'    => 'slug',
+				'terms'    => 'draft',
+				'operator' => 'NOT IN',
+			],
+		] );
+	}
+	return $query;
+}
